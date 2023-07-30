cf5a369ff9cf4698116d5e63eb7f096d71b5e07a
heinlyLewier <armandolewier877@gmail.com>
2023-07-30 16:45:34 +0900

    Skripsi

--- application/views/v_kos_terbaru.php
+++ application/views/v_kos_terbaru.php
@@ -50,7 +50,7 @@
                                 </table>
                                 <hr>
                                 <div class="btn-group" role="group" aria-label="Basic example">
-                                    <a name="" id="" class="btn btn-sm btn-primary border-0" href="<?= site_url('peta/lokasi/' . $value->id_kos); ?>" role="button"><i class="fas fa-route"></i> Lokasi</a>
+                                    <a name="" id="" class="btn btn-sm btn-primary border-0" href="<?= site_url('peta/lokasi/' . $value->id_kos); ?>" role="button"><i class="fas fa-route"></i> Lihat Lokasi</a>
                                     <a name="" id="" class="btn btn-warning btn-sm" href="<?= site_url('terbaru/detail/' . $value->id_kos); ?>" role="button"><i class="fa fa-eye" aria-hidden="true"></i> Detail</a>
                                     <a name="" id="" class="btn btn-sm btn-success border-0" style="background-color: #298505;" href="http:///api.whatsapp.com/send?phone=<?= $value->no_wa; ?>&text=Halo%20Admin,%20Saya%20Mau%20Pesan%20Kamar." role="button" target="_blank"><i class="fab fa-whatsapp"></i> Whatsapp</a>
                                 </div>
